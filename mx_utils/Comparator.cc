55bd010745e475a63f25225b74a68f24951a4728
Yongning <yongning.fan@gmail.com>
2019-02-07 19:53:45 +0800

    2nd draft

--- mx_utils/Comparator.cc
+++ mx_utils/Comparator.cc
@@ -31,12 +31,40 @@ public:
         while ((diff_index < min_length) && 
             ((*start)[diff_index] == limit[diff_index])) {
                 diff_index++;
-            }
+        }
         
         if (diff_index >= min_length) {
 
+        } else {
+            uint8_t diff_byte = static_cast<uint8_t>((*start)[diff_index]);
+            if (diff_byte < static_cast<uint8_t)(0xff) &&
+                diff_byte + 1 < static_cast<uint8_t>(limit[diff_index])) {
+                    (*start)[diff_index]++;
+                    start->resize(diff_index + 1);
+                    assert(Compare(*start, limit) < 0);
+                }
+        }
+    }
+
+    // find first character that can be incremented
+    virtual void FindShortSuccessor(std::string* key) const {
+        size_t n = key->size();
+        for (size_t i = 0; i < n; i++) {
+            const uint8_t byte = (*key)[i];
+            if (byte != static_cast<uint8_t>(0xff)) {
+                (*key)[i] = byte + 1);
+                key->resize(i + 1);
+                return;
+            }
         }
     }
 }
+
 }
+
+const Comparator* BytewiseComparator() {
+    static NoDestructor<BytewiseComparatorImpl> singleton;
+    return singleton.get();
+}
+
 }
